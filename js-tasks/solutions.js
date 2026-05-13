e3de5a2b0b1d8e089c90cb2915605909ccd2526f
lx-aliaksandrtsimoshyn <aliaksandr.tsimoshyn@leverx.com>
2026-05-13 23:24:40 +0300

    add comments to identifyTypes function for clarity

--- js-tasks/solutions.js
+++ js-tasks/solutions.js
@@ -1,5 +1,10 @@
 // Core Concepts
 
+// Identify Data Types
+// Write a function that returns the type of each value in an array.
 function identifyTypes(arr) {
   return arr.map((value) => typeof value)
 }
+console.log(identifyTypes([1, 'hello', true, null, undefined, Symbol(), BigInt(10), {}]));
+
+///////////////////////////////////////////////////////////////////////////////////////////
